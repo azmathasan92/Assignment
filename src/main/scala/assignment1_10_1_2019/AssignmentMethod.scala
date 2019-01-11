28e359dbba8e01b5c88fabdfd6c5efe2ba036e61
azmathasan92 <azmat.hasan@knoldus.in>
2019-01-11 13:31:46 +0530

    Done Assignment-1 Work

--- src/main/scala/assignment1_10_1_2019/AssignmentMethod.scala
+++ src/main/scala/assignment1_10_1_2019/AssignmentMethod.scala
@@ -1,6 +1,36 @@
 package assignment1_10_1_2019
 
 class AssignmentMethod {
+  def printIndexWithEachElement(list: List[Int]): List[Int] = {
+    def printElement(elements: List[Int], list: List[Int]): List[Int] = {
+      elements match {
+        case Nil =>
+          list
+        case head :: tail => val index = indexOfElement(head, list)
+          println(s"$index = $head")
+          printElement(tail, list)
+        case _ => throw new NoSuchElementException
+      }
+    }
+    printElement(list, list)
+  }
+  def indexOfElement(element: Int, list: List[Int]): Int = {
+    def findIndex(index: Int, list: List[Int]): Int = {
+      list match {
+        case head :: Nil => if(head == element) index
+        else throw new NoSuchElementException
+        case head :: tail => if(head == element) index
+        else findIndex(index + 1, tail)
+        case _ => throw new NoSuchElementException
+      }
+    }
+    findIndex(0, list)
+  }
+  def maximumNumberInList(list: List[Int]): Option[Int] = list match {
+    case Nil => None
+    case List(x: Int) => Some(x)
+    case x :: y :: rest => maximumNumberInList( (if (x > y) x else y) :: rest )
+  }
   def getFibonacci(index: Int): Int = {
     if (index <= 0) {
       0
@@ -19,4 +49,5 @@ class AssignmentMethod {
     }
     go(1, n)
   }
+
 }
--- src/main/scala/assignment1_10_1_2019/ListUsingStringInterpolation.scala
+++ src/main/scala/assignment1_10_1_2019/ListUsingStringInterpolation.scala
@@ -2,20 +2,9 @@ package assignment1_10_1_2019
 
 object ListUsingStringInterpolation extends App {
   val list: List[Int] = List(10, 50, 30, 40)
-  var max:Int=0;
-  list.foreach {
-    number =>println(s"${list.indexOf(number)} = $number")
-      if(max<number){max=number}
-  }
-  println("Maximum Number from list is "+max)
-
-  val fibonacci=new AssignmentMethod
-  println(fibonacci.getFibonacci(2))
-  println(fibonacci.getFibonacci(3))
-  println(fibonacci.getFibonacci(4))
-  println(fibonacci.getFibonacci(5))
-
-
-
-  println(fibonacci.getFactorial(5))
+  val assignment=new AssignmentMethod
+  println(assignment.printIndexWithEachElement(list))
+  println(s"Maximum Number from list is ${assignment.maximumNumberInList(list)} ")
+  println(s"fobonacci = ${assignment.getFibonacci(5)}")
+  println(s"factorial of a number is ${assignment.getFactorial(5)}")
 }
